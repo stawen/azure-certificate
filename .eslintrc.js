44a2b9dd6b155566e3b921456d068005d6dbc2bc
Stawen <stawen@dronek.com>
2023-01-07 10:39:20 +0100

    test new script

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,25 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-    extends: ['plugin:prettier/recommended', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'prettier',
+    ],
     rules: {
         '@typescript-eslint/no-inferrable-types': 'off',
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
     },
 }
